99c86b441dceb5911b50e764e1460d02bf13faf7
AnPQ <anpham1122000@gmail.com>
2024-12-31 00:06:22 +0700

    chat-app-server

---
+++ chat-app/server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server listening on port: http://localhost:${port}`);
+});
+
+app.get("/", (req, res) => res.send("Welcome to chat server"));
+
+const uri = process.env.MONGO_URI;
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connection established");
+  })
+  .catch((error) => console.log(error.message));
